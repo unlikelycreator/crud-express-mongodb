b973c880926d6e07bd06041768d40b87481f3304
Hritik Pawar <73632558+unlikelycreator@users.noreply.github.com>
2020-11-04 20:09:19 +0530

    retrieved data not displayed

---
+++ Trash/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express();
+
+
+
+MongoClient.connect('mongodb+srv://Ampfibian:pawar2700@cluster0.vmmbl.mongodb.net/<dbname>?retryWrites=true&w=majority', { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    app.use(bodyParser.urlencoded({ extended: true }))
+
+    app.get('/', (req, res) => {
+  	res.sendFile('C:/Hritik/programming/Advanced Web/FirstMern' + '/index.html')
+	})
+
+	app.post('/quotes', (req, res) => {
+  	console.log(req.body)
+  	})
+
+  	app.listen(3000, function() {
+	console.log('listening on 3000')
+	})
+	})
+  .catch(console.error)
+
+//app.get('/', (req, res) => {/*...*/})
+//app.post('/quotes', (req, res) => {/*...*/})
+
+
+
--- server.js
+++ server.js
@@ -1,36 +1,38 @@
 const express = require('express');
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express();
 
 
+MongoClient.connect('mongodb+srv://Ampfibian:pawar2700@cluster0.vmmbl.mongodb.net/<dbname>?retryWrites=true&w=majority', { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('star-wars-quotes')
+        app.set('view engine', 'ejs')
+        app.use(bodyParser.urlencoded({ extended: true }))
 
-MongoClient.connect('mongodb+srv://Username:password@cluster0.vmmbl.mongodb.net/<dbname>?retryWrites=true&w=majority', { useUnifiedTopology: true })
-  .then(client => {
-    console.log('Connected to Database')
-    const db = client.db('star-wars-quotes')
-    app.use(bodyParser.urlencoded({ extended: true }))
-
-    app.get('/', (req, res) => {
-  	res.sendFile('D:/Programming/WEB/MERN/crud-express-nodejs' + '/index.html')
-	})
-
-	app.post('/quotes', (req, res) => {
-        db.collection('quotes').insertOne(req.body, (err, result) => {
-          if (err) return console.log(err)
-          console.log('saved to database')
-          res.redirect('/')
+        app.get('/', (req, res) => {
+            res.sendFile('C:/Hritik/programming/Advanced Web/FirstMern' + '/index.html')
+            db.collection('quotes').find().toArray()
+                .then(results => {
+                    console.log(results)
+                })
+                .catch(error => console.error(error))
+            res.render('index.ejs', {})
         })
-      })
+        app.post('/quotes', (req, res) => {
+            db.collection('quotes').insertOne(req.body, (err, result) => {
+                if (err) return console.log(err)
+                console.log('saved to database')
+                res.redirect('/')
+            })
+        })
 
-  	app.listen(3000, function() {
-	console.log('listening on 3000')
-	})
-	})
-  .catch(console.error)
+        app.listen(3000, function() {
+            console.log('listening on 3000')
+        })
+    })
+    .catch(console.error)
 
 //app.get('/', (req, res) => {/*...*/})
 //app.post('/quotes', (req, res) => {/*...*/})
-
-
-
